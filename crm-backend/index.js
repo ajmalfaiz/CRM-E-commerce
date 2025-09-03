812ffef6b0e2d8eed237780c3fa458312237cb24
Mani Kant <152617394+manikant97@users.noreply.github.com>
2025-09-03 11:25:25 +0530

    update the origin

--- crm-backend/index.js
+++ crm-backend/index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 
 // Configure CORS
 const corsOptions = {
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'https://crm-e-commerce.vercel.app/', // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
